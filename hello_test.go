54a2988ec61cdcef65304771857294a8f84f8f0e
Ardi Ismail <>
2019-09-13 13:46:35 +0700

    add spain, bahasa, french

--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,32 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Ardi")
+		got := Hello("Ardi", "")
 		want := "Hello, Ardi"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Ardi", "Spanish")
+		want := "Hola, Ardi"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Ardi", "French")
+		want := "Bonjour, Ardi"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in indonesia", func(t *testing.T) {
+		got := Hello("Ardi", "Indonesia")
+		want := "Halo, Ardi"
 		assertCorrectMessage(t, got, want)
 	})
 }
