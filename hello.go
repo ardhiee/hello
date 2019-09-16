be74e130d1ef01d095e332ebaf5863d01304f6c7
Ardi Ismail <>
2019-09-17 06:25:15 +0700

    add func Hello and unit test

--- hello.go
+++ hello.go
@@ -1,34 +1,5 @@
 package hello
 
-import "fmt"
-
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const french = "French"
-const spanish = "Spanish"
-
-// Hello func only return string
-func Hello(name, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
-	case french:
-		prefix = frenchHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-	return prefix
-}
-
-func main() {
-	fmt.Println(Hello("Ardi", ""))
+func Hello() string {
+	return "Hello, World"
 }
--- hello_test.go
+++ hello_test.go
@@ -4,34 +4,12 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	assertCorrectMessage := func(t *testing.T, got, want string) {
-		t.Helper()
+	t.Run("will return simple hello world", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, World"
+
 		if got != want {
 			t.Errorf("got %q wants %q", got, want)
 		}
-	}
-
-	t.Run("Can receive name parameter", func(t *testing.T) {
-		got := Hello("Ardi", "")
-		want := "Hello, Ardi"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Have a default value where name is empty", func(t *testing.T) {
-		got := Hello("", "")
-		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in spanish additional language support", func(t *testing.T) {
-		got := Hello("Ardi", "Spanish")
-		want := "Hola, Ardi"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in french additional language support", func(t *testing.T) {
-		got := Hello("Ardi", "French")
-		want := "Bonjour, Ardi"
-		assertCorrectMessage(t, got, want)
 	})
 }
