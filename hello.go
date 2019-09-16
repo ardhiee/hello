3ef443e2f91bbb285582cdc1fbfd336afec48da4
Ardi Ismail <>
2019-09-16 22:28:22 +0700

    refactor using switch case, add new greeting func

--- hello.go
+++ hello.go
@@ -5,6 +5,8 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
+const french = "French"
+const spanish = "Spanish"
 
 // Hello func only return 'Hello, World!'
 func Hello(name, language string) string {
@@ -12,16 +14,20 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World!"
 	}
+	return greetingPrefix(language) + name
+}
 
-	if language == "Spanish" {
-		return spanishHelloPrefix + name
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	if language == "French" {
-		return frenchHelloPrefix + name
-	}
-
-	return englishHelloPrefix + name
+	return prefix
 }
 
 func main() {
