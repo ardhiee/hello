2020a37699c665f2e17280b8e6429ba5deda0c9c
Ardi Ismail <>
2019-09-17 06:33:02 +0700

    add default value for Hello

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
 package hello
 
+const englishHelloPrefix = "Hello, "
+
 // Hello function just give simple output
 func Hello(name string) string {
 
 	if name == "" {
 		name = "World"
 	}
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
