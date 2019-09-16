2284fc1ebdc5665eab38778eded3c608ab0a32ae
Ardi Ismail <>
2019-09-16 22:00:36 +0700

    Test Day 2-A, add hello world and unit test

--- hello.go
+++ hello.go
@@ -1,40 +1,11 @@
-package main
+package hello
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const indonesia = "Indonesia"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const indonesiaHelloPrefix = "Halo, "
-
-// Hello func only receive string and return string
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "world"
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	case indonesia:
-		prefix = indonesiaHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-
-	return
+func Hello() string {
+	return "Hello, World!"
 }
 
 func main() {
-	fmt.Println(Hello("Ardi", ""))
+	fmt.Println(Hello())
 }
