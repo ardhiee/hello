05b9a8355be7b22753f752479419140da1d12568
Ardi Ismail <>
2019-09-26 17:55:11 +0700

    add language func

--- hello.go
+++ hello.go
@@ -1,9 +1,27 @@
 package hello
 
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const franceHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const france = "France"
+
 // Hello func just give some output
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return "Hello, " + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case france:
+		prefix = franceHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
--- hello_test.go
+++ hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("say hello", func(t *testing.T) {
-		got := Hello("Ardi")
+		got := Hello("Ardi", "")
 		want := "Hello, Ardi"
 
 		if got != want {
@@ -14,8 +14,27 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say hello return default value world", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say hello in spanish", func(t *testing.T) {
+		got := Hello("Ardi", "Spanish")
+		want := "Hola, Ardi"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+
+	t.Run("say hello in france", func(t *testing.T) {
+		got := Hello("Ardi", "France")
+		want := "Bonjour, Ardi"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
